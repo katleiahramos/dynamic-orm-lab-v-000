9497f341f81534564edcee58e0f2feb530db4a1f
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-12 01:45:14 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,7 +57,9 @@ def self.column_names
 
     attribute_key = attribute.keys[0].to_s
     attribute_value = attribute.values[0]
+    
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_key} = #{attribute_value}"
+    binding.pry
     DB[:conn].execute(sql)
   end
 end
